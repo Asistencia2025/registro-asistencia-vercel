6fce6a4029ea995c04ad147d756ba85043b0e790
Asistencia 2025 <comunicacion2rr@gmail.com>
2025-08-19 07:32:53 -0500

    Desactivar regla no-explicit-any en ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Reglas personalizadas
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Desactiva la regla de "any"
+    },
+  },
 ];
 
 export default eslintConfig;
